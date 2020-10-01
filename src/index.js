a0f15a27cecd31199f92b3fd21f20a8213db6972
vladimirm85 <vladimirm85@gmail.com>
2020-10-01 23:37:43 +0300

    feat: min implemented

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = (array) => {
+  if (!array || !array.length) return 0;
+  array.sort((a, b) => a - b);
+  return array[0];
 }
 
 exports.max = function max (array) {
